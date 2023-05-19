476bdde5a338d1afd88b915f6783e03aa5bdc209
GabrielmAlves <gabrielmalves10@gmail.com>
2023-05-19 15:59:00 -0300

    Projeto finalizado mas farei eventuais alterações no futuro. Adicionei sons aos botões e fiz ajustes na lógica.

--- js/script.js
+++ js/script.js
@@ -1,22 +1,72 @@
 const nomePokemon = document.querySelector('.nomePoke');
 const numeroPokemon = document.querySelector('.numeroPoke');
 const imagemPokemon = document.querySelector('.imagemPoke');
+const proximo = document.querySelector('.btnProximo');
+const anterior = document.querySelector('.btnAnterior');
+const shiny = document.querySelector('.btnShiny');
+const form = document.querySelector('.form');
+const input = document.querySelector('.inputForm');
+
+let atual = 1;
 
 const acharPokemon = async (pokemon) => {
     /*seeking the pokemon inside the API*/ 
     const API = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
     
-    const dadosPokemon = await API.json() /*turning the data into json*/ 
-    return dadosPokemon;
+    if(API.status === 200){
+        const dadosPokemon = await API.json() /*turning the data into json*/ 
+        return dadosPokemon;
+    }else{
+        imagemPokemon.src = '';
+        atual = 1;
+        nomePokemon.innerHTML = 'Não encontrado!';
+    }
 }
 
 const renderizaPoke = async (pokemon) => {
-
+    nomePokemon.innerHTML = 'Procurando...';
+    numeroPokemon.innerHTML = '';
     const dados = await acharPokemon(pokemon);
 
-    numeroPokemon.innerHTML = dados.id;
-    nomePokemon.innerHTML = dados.name;
-    imagemPokemon.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+    if(dados){
+        numeroPokemon.innerHTML = dados.id;
+        nomePokemon.innerHTML = dados.name;
+        atual = dados.id;
+        imagemPokemon.src = dados['sprites']['versions']['generation-v']['black-white']['animated']['front_default'];
+        input.value= '';
+    }
 }
 
-renderizaPoke('7');
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+    inputEnviado = input.value; 
+    renderizaPoke(inputEnviado.toLowerCase());
+    console.log(inputEnviado);
+    input.value= '';
+});
+
+proximo.addEventListener('click', () => {
+   var audio = document.getElementById('meuAudio');
+   audio.play();
+   atual++;
+   renderizaPoke(atual);
+   
+});
+
+anterior.addEventListener('click', () => {
+    if(atual > 1){
+    var audio = document.getElementById('meuAudio');
+    audio.play();
+    atual--;
+    renderizaPoke(atual);
+    }else{
+        alert('Não existe o Pokémon anterior!');
+    }
+});
+
+shiny.addEventListener('click', () => {
+    alert('Funcionalidade em desenvolvimento!!');
+});
+
+
+renderizaPoke(atual);
